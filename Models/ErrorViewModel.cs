538ae61ecd05ff4f830f418d2cb758ff3f1a02bb
Rogerio <rogerio.santana@df.docente.senai.br>
2023-08-30 17:50:47 -0300

    implementando software

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SaborCriativo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
